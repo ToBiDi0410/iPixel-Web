04bcbf577cd15875d2f4382d8eff67f42612ff51
ToBiDi0410 <40767401+ToBiDi0410@users.noreply.github.com>
2025-10-22 12:36:01 +0200

    Added support for more commands

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,49 @@
 <script setup>
 import { ref } from 'vue';
-import { AppState, selectDevice, ledOff, ledOn, setTime, setClockMode } from './AppState';
+import { AppState, selectDevice, ledOff, ledOn, setTime, setClockMode, setOrientation, setBrightness, setFunMode, clear, deleteScreen, setRythmAnimationMode, setRythmLevelMode, sendText } from './AppState';
 
-const setClockMode_style = ref(0);
+const setClockMode_style = ref(1);
 const setClockMode_dayOfWeek = ref(new Date().getDay());
 const setClockMode_year = ref(new Date().getFullYear() - 2000);
 const setClockMode_month = ref(new Date().getMonth());
 const setClockMode_day = ref(new Date().getDate());
 const setClockMode_showDate = ref(1);
 const setClockMode_format24 = ref(1);
+
+const setOrientation_value = ref(0);
+const setBrightness_value = ref(0);
+const setSpeed_value = ref(0);
+const deleteScreen_value = ref(0);
+
+const setRythmAnimationMode_style = ref(0);
+const setRythmAnimationMode_frame = ref(0);
+
+const setRythmLevelMode_style = ref(1);
+const setRythmLevelMode_level0 = ref(0);
+const setRythmLevelMode_level1 = ref(0);
+const setRythmLevelMode_level2 = ref(0);
+const setRythmLevelMode_level3 = ref(0);
+const setRythmLevelMode_level4 = ref(0);
+const setRythmLevelMode_level5 = ref(15);
+const setRythmLevelMode_level6 = ref(0);
+const setRythmLevelMode_level7 = ref(0);
+const setRythmLevelMode_level8 = ref(0);
+const setRythmLevelMode_level9 = ref(0);
+const setRythmLevelMode_level10 = ref(0);
+
+const setText_text = ref("Hello");
+const setText_rainbowMode = ref(0);
+const setText_animation = ref(0);
+const setText_saveSlot = ref(1);
+const setText_speed = ref(80);
+const setText_colorR = ref(255);
+const setText_colorG = ref(255);
+const setText_colorB = ref(255);
+const setText_font = ref("default")
+const setText_matrixHeight = ref(16);
+const setText_fontSize = ref(0);
+const setText_fontOffsetX = ref(0);
+const setText_fontOffsetY = ref(0);
 
 const setTime_hour = ref(0);
 const setTime_minute = ref(0);
@@ -25,12 +60,38 @@ const setTime_second = ref(0);
 
         <div v-if="AppState.negotiateState == 'READY'">
             <h1>Controls</h1>
-            <h2>Power</h2>
-            <button @click="ledOn()" v-if="AppState.negotiateState == 'READY'">Led On</button>
-            <br>
-            <button @click="ledOff()" v-if="AppState.negotiateState == 'READY'">Led Off</button>
 
-            <h2>Clock</h2>
+            <!-- Power -->
+            <h2>Power</h2>
+            <div>
+                <button @click="ledOn()">Led On</button>
+            </div>
+            <div>
+                <button @click="ledOff()">Led Off</button>
+            </div>
+
+            <!-- Orientation -->
+            <h2>Orientation</h2>
+            <div>
+                <input type="number" min="0" max="3" placeholder="Orientation" v-model="setOrientation_value" />
+                <button @click="setOrientation(setOrientation_value)">Set Orientation</button>
+            </div>
+
+            <!-- Brightness -->
+            <h2>Brightness</h2>
+            <div>
+                <input type="number" min="0" max="100" placeholder="Brightness" v-model="setBrightness_value" />
+                <button @click="setBrightness(setBrightness_value)">Set Brightness</button>
+            </div>
+
+            <!-- Speed -->
+            <h2>Speed</h2>
+            <div>
+                <input type="number" min="0" max="100" placeholder="Speed" v-model="setSpeed_value" />
+                <button @click="setSpeed(setSpeed_value)">Set Speed</button>
+            </div>
+
+            <h2>Clock Mode</h2>
             <!-- Clock Mode -->
             <div>
                 <input type="number" min="1" max="8" placeholder="Style" v-model="setClockMode_style" />
@@ -48,9 +109,74 @@ const setTime_second = ref(0);
                 <input type="number" min="0" max="23" placeholder="Hour" v-model="setTime_hour" />
                 <input type="number" min="0" max="59" placeholder="Minute" v-model="setTime_minute" />
                 <input type="number" min="0" max="59" placeholder="Second" v-model="setTime_second" />
-                <button @click="setTime(setTime_hour, setTime_minute, setTime_second)" v-if="AppState.negotiateState == 'READY'">Set Time</button>
+                <button @click="setTime(setTime_hour, setTime_minute, setTime_second)">Set Time</button>
             </div>
 
+            <!-- Fun Mode -->
+            <h2>Fun Mode</h2>
+            <div>
+                <button @click="setFunMode(true)">Fun Mode On</button>
+            </div>
+            <div>
+                <button @click="setFunMode(false)">Fun Mode Off</button>
+            </div>
+
+            <!-- Rythm Mode -->
+            <h2>Rythm Mode</h2>
+            <div>
+                <input type="number" min="0" max="1" placeholder="Style" v-model="setRythmAnimationMode_style" />
+                <input type="number" min="0" max="7" placeholder="Frame" v-model="setRythmAnimationMode_frame" />
+                <button @click="setRythmAnimationMode(setRythmAnimationMode_style, ++setRythmAnimationMode_frame)">+ Frame</button>
+                <button @click="setRythmAnimationMode(setRythmAnimationMode_style, --setRythmAnimationMode_frame)">- Frame</button>
+                <button @click="setRythmAnimationMode(setRythmAnimationMode_style, setRythmAnimationMode_frame)">Set Level Rythm Animation</button>
+            </div>
+
+            <div>
+                <input type="number" min="1" max="4" placeholder="Style" v-model="setRythmLevelMode_style" />
+                <input type="number" min="0" max="15" placeholder="Level 0" v-model="setRythmLevelMode_level0" />
+                <input type="number" min="0" max="15" placeholder="Level 1" v-model="setRythmLevelMode_level1" />
+                <input type="number" min="0" max="15" placeholder="Level 2" v-model="setRythmLevelMode_level2" />
+                <input type="number" min="0" max="15" placeholder="Level 3" v-model="setRythmLevelMode_level3" />
+                <input type="number" min="0" max="15" placeholder="Level 4" v-model="setRythmLevelMode_level4" />
+                <input type="number" min="0" max="15" placeholder="Level 5" v-model="setRythmLevelMode_level5" />
+                <input type="number" min="0" max="15" placeholder="Level 6" v-model="setRythmLevelMode_level6" />
+                <input type="number" min="0" max="15" placeholder="Level 7" v-model="setRythmLevelMode_level7" />
+                <input type="number" min="0" max="15" placeholder="Level 8" v-model="setRythmLevelMode_level8" />
+                <input type="number" min="0" max="15" placeholder="Level 9" v-model="setRythmLevelMode_level9" />
+                <input type="number" min="0" max="15" placeholder="Level 10" v-model="setRythmLevelMode_level10" />
+                <button @click="setRythmLevelMode(setRythmLevelMode_style, [setRythmLevelMode_level0, setRythmLevelMode_level1, setRythmLevelMode_level2, setRythmLevelMode_level3, setRythmLevelMode_level4, setRythmLevelMode_level5, setRythmLevelMode_level6, setRythmLevelMode_level7, setRythmLevelMode_level8, setRythmLevelMode_level9, setRythmLevelMode_level10])">Set Rythm Levels</button>
+            </div>
+
+            <!-- Text -->
+            <h2>Text (Broken)</h2>
+            <div>
+                <input type="text" minlength="0" maxlength="20" placeholder="Text" v-model="setText_text" />
+                <input type="number" min="0" max="9" placeholder="Rainbow Mode" v-model="setText_rainbowMode" />
+                <input type="number" min="0" max="7" placeholder="Animation" v-model="setText_animation" />
+                <input type="number" min="0" max="10" placeholder="Slot" v-model="setText_saveSlot" />
+                <input type="number" min="0" max="100" placeholder="Speed" v-model="setText_speed" />
+                <input type="number" min="0" max="255" placeholder="Color R" v-model="setText_colorR" />
+                <input type="number" min="0" max="255" placeholder="Color G" v-model="setText_colorG" />
+                <input type="number" min="0" max="255" placeholder="Color B" v-model="setText_colorB" />
+                <input type="text" placeholder="Font" v-model="setText_font" />
+                <input type="number" min="1" max="128" placeholder="Matrix Height" v-model="setText_matrixHeight" />
+                <input type="number" min="0" max="255" placeholder="Font Size" v-model="setText_fontSize" />
+                <input type="number" min="0" max="255" placeholder="Font Offset X" v-model="setText_fontOffsetX" />
+                <input type="number" min="0" max="255" placeholder="Font Offset Y" v-model="setText_fontOffsetY" />
+                <button @click="sendText(setText_text, setText_rainbowMode, setText_animation, setText_saveSlot, setText_speed, setText_colorR, setText_colorG, setText_colorB, setTExt_font, setText_matrixHeight, setText_fontSize, setText_fontOffsetX, setText_fontOffsetY)">Set Text</button>
+                <p>text, rainbowMode, animation, saveSlot, speed,r ,g ,b, font, matrixHeight, fontSize, fontOffsetX, fontOffsetY</p>
+
+            </div>
+
+            <!-- EEPROM -->
+            <h2>EEPROM</h2>
+            <div>
+                <input type="number" min="0" max="10" placeholder="Screen" v-model="deleteScreen_value" />
+                <button @click="deleteScreen(deleteScreen_value)">Delete Screen</button>
+            </div>
+            <div>
+                <button @click="clear()">Clear EEPROM</button>
+            </div>
         </div>        
     </div>
 </template>
--- src/AppState.js
+++ src/AppState.js
@@ -78,15 +78,87 @@ export async function sendCommand(bytes) {
     console.log("Sent", bytes);
 }
 
-export async function ledOn() {
-    await sendCommand(new Uint8Array([0x05, 0x00, 0x07, 0x01, 0x01]));
+
+export async function setTime(hour, minute, second) {
+    await sendCommand(new Uint8Array([0x08, 0x00, 0x01, 0x80, hour, minute, second, 0x00]));
+}
+export async function setFunMode(value) {
+    await sendCommand(new Uint8Array([0x05, 0x00, 0x04, 0x01, value ? 1 : 0]));
+}
+export async function setOrientation(orientation) {
+    await sendCommand(new Uint8Array([0x05, 0x00, 0x06, 0x80, orientation]));
+}
+export async function clear() {
+    await sendCommand(new Uint8Array([0x04, 0x00, 0x03, 0x80]));
+}
+export async function setBrightness(brightness) {
+    await sendCommand(new Uint8Array([0x05, 0x00, 0x04, 0x80, brightness]));
+}
+export async function setSpeed(speed) {
+    await sendCommand(new Uint8Array([0x05, 0x00, 0x03, speed]));
 }
 export async function ledOff() {
     await sendCommand(new Uint8Array([0x05, 0x00, 0x07, 0x01, 0x00]));
 }
+export async function ledOn() {
+    await sendCommand(new Uint8Array([0x05, 0x00, 0x07, 0x01, 0x01]));
+}
+export async function deleteScreen(screen) {
+    await sendCommand(new Uint8Array([0x07, 0x00, 0x02, 0x01, 0x00, screen]));
+}
+export async function setPixel(x, y, r, g, b) {
+    await sendCommand(new Uint8Array([0x0A, 0x00, 0x05, 0x01, 0x00, r, g, b, x, y]));
+}
 export async function setClockMode(style, dayOfWeek, year, month, day, showDate, format24) {
     await sendCommand(new Uint8Array([0x0B, 0x00, 0x06, 0x01, style, dayOfWeek, year, month, day, showDate ? 1 : 0, format24 ? 1 : 0]));
 }
-export async function setTime(hour, minute, second) {
-    await sendCommand(new Uint8Array([0x08, 0x00, 0x01, 0x80, hour, minute, second, 0x00]));
+export async function setRythmLevelMode(style, levels) {
+    await sendCommand(new Uint8Array([0x10, 0x00, 0x01, 0x02, style, ...levels.slice(0, 10)]));
+}
+export async function setRythmAnimationMode(style, frame) {
+    await sendCommand(new Uint8Array([0x06, 0x00, 0x00, 0x02, frame, style]));
+}
+export async function sendText(text, rainbowMode, animation, saveSlot, speed,
+                            r, g ,b, font, matrixHeight, fontSize,
+                            fontOffsetX, fontOffsetY) {
+    await sendCommand(generateTextBuffer(text, rainbowMode, animation, saveSlot, speed,
+                            r, g, b, font, matrixHeight, fontSize,
+                            fontOffsetX, fontOffsetY));
+}
+
+export function generateTextBuffer(text, rainbowMode, animation, saveSlot, speed,
+                            r, g, b, font, matrixHeight, fontSize,
+                            fontOffsetX, fontOffsetY) {
+  // Convert to string, truncate text length to 120
+  text = String(text || "");
+  let tlen = Math.min(text.length, 120);
+
+  const headerLen = 14;
+  let packetLen = headerLen + tlen;
+  if (packetLen > 240) packetLen = 240;
+
+  const buf = new Uint8Array(packetLen);
+
+  buf[0] = 0xAA;
+  buf[1] = 0x02;
+  buf[2] = tlen & 0xFF;
+  buf[3] = rainbowMode & 0xFF;
+  buf[4] = animation & 0xFF;
+  buf[5] = saveSlot & 0xFF;
+  buf[6] = speed & 0xFF;
+  buf[7] = r & 0xFF;
+  buf[8] = g & 0xFF;
+  buf[9] = b & 0xFF;
+
+  buf[10] = matrixHeight & 0xFF;
+  buf[11] = fontSize & 0xFF;
+  buf[12] = fontOffsetX & 0xFF;
+  buf[13] = fontOffsetY & 0xFF;
+
+  // Copy text as ASCII
+  for (let i = 0; i < tlen && headerLen + i < packetLen; i++) {
+    buf[headerLen + i] = text.charCodeAt(i) & 0xFF;
+  }
+
+  return buf;
 }
